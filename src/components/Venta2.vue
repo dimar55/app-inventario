1c10cbca1db4596d7817c3ff955b728470acc401
Schied <davidcelis682@gmail.com>
2022-12-10 00:35:06 -0500

    Update

--- src/components/Venta2.vue
+++ src/components/Venta2.vue
@@ -1,19 +1,64 @@
 <template>
+     <transition name="fade">
+        <div class="modal-overlay" v-if="showModal"></div>
+    </transition>
+    <transition name="fade">
+        <div class="container-registro modal" v-if="showModal">
+            <form class="ctn-registrar"  v-on:submit.prevent="registrarCliente">
+                <div class="container-flex">
+                    <div>
+                    </div>  
+                    <div>
+                        <img src="../assets/icon_X.png" alt="" @click="showModal = false">
+                    </div>    
+            </div>
+            <h1>Registrar cliente</h1>
+                <div class="campos_registrar">
+                    <img src="../assets/cliente.png" alt="">
+                    <p>Nombre</p>
+                    <input type="text" v-model="ClienteForm.nombre_cli" required>
+                </div>
+                <div class="campos_registrar">
+                    <p>Tipo de Documento</p>
+                    <select v-model="ClienteForm.tipo_documento_cli" required>
+                        <option>CC Cedula de Ciudadania</option>
+                        <option>TT Tarjeta de Identidad</option>
+                        <option>CE Cedula de Extranjeria</option>
+                    </select>
+                </div>
+                <div class="campos_registrar">
+                    <p>Numero de Documento:</p>
+                    <input type="number" v-model="ClienteForm.cedula_cli" required>
+                </div>
+                <div class="campos_registrar">
+                    <p>Telefono:</p>
+                    <input type="number" v-model="ClienteForm.telefono_cli" required>
+                </div>
+                <div class="campos_registrar">
+                    <p>Dirección:</p>
+                    <input type="text" v-model="ClienteForm.direccion_cli" required>
+                </div>  
+                <div class="boton">
+                    <button type="submit" class="btn">REGISTRAR</button>
+                </div>
+            </form>
+        </div>
+    </transition>
     <div class="ctn-venta">
         <div class="titulo">
             <h1> VENTA</h1>
             <form class="filtro"  v-on:submit.prevent="buscarProducto">
-                <input type="text" placeholder="Buscar Producto">
+                <input type="text" placeholder="Buscar Producto" v-model="id_producto">
             </form>
         </div>
         <div class="ctn-venta">
             <div class="ctn-registrar-saldo">
                 <form class="filtro" v-on:submit.prevent="buscarCliente">
-                    <input type="number" placeholder="Cedula cliente"  required>
+                    <input type="number" placeholder="Cedula cliente" v-model="cedula_cli" required>
                     <button class="btn" type="submit">BUSCAR</button>
                 </form>
                 <br>
-                <div class="ctn-pro" v-show="clienteExiste2">
+                <div class="ctn-pro" v-show="clienteExiste">
                     <p>Cliente no registrado</p>
                     <button class="btn" @click="goCliente()">REGISTRAR</button>
                 </div>
@@ -83,12 +128,11 @@
                         <div class="cmp">
                             <div>
                                 <label>CC: </label>
-                                <label>1190234210</label>
+                                <label>{{this.cliente.cedula_cli}}</label>
                             </div>
-
                             <div>
                                 <label>Nombre: </label>
-                                <label>Juna Alberto Ramirez Posada</label>
+                                <label>{{this.cliente.nombre_cli}}</label>
                             </div>
                         </div>
                     </div>
@@ -105,32 +149,262 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Harina Arepasan 1 KG</td>
-                                <td>1123123</td>
-                                <td>-3+</td>
-                                <td>3.000</td>
-                                <td>9.000</td>
-                                <td>X</td>
+                            <tr v-for="(prod, index) in prods_venta" :key="prod.nombre_product">
+                                <td>{{prod.nombre_product}} {{prod.marca_product}} {{prod.cantidad_product}}{{prod.unidad_poduct}}</td>
+                                <td>{{prod.id_product}}</td>
+                                <td><span class="cursor" @click="resCantidad(prod.id_product)">-</span> {{prod.cant_venta}} <span class="cursor" @click="sumCantidad(prod.id_product)">+</span></td>
+                                <td>{{prod.precio_venta}}</td>
+                                <td>{{prod.cant_venta*prod.precio_venta}}</td>
+                                <td><span class="cursor" @click="eliminarProd(index)">X</span></td>
                             </tr>
                         </tbody>
                     </table>
-                   <label>total: 9.000</label>
+                   <label>total: {{formatoMoneda(this.total)}}</label>
                 </div>
-                <button class="btn" type="submit">REGISTRAR VENTA</button>
-                <button class="btn" type="submit">ABRIR SALDO</button>
+                <button class="btn" @click="registrarVenta(false)">REGISTRAR VENTA</button>
+                <button class="btn" @click="registrarVenta(true)">ABRIR SALDO</button>
             </div>
         </div>
     </div>  
 </template>
 
 <script>
+import axios from "axios";
+import Swal from "sweetalert2";
+import config from '../utils/utils';
 export default{
-    name: 'Venta2'
+    name: 'Venta2',
+    data(){
+        return {
+            showModal: false,
+            filtrado: false,
+            id_producto: "",
+            total:  0,
+            prods_filtro: [],
+            prods_venta: [],
+            cedula_cli: '',
+            clienteExiste: false,
+            cliente: {
+                cedula_cli: '',
+                nombre_cli: ''
+            },
+            ClienteForm: {
+               cedula_cli: "",
+               nombre_cli: "",
+               tipo_documento_cli: "",
+               telefono_cli: "",
+               direccion_cli: ""
+            }
+
+        }
+    },
+    methods: {
+        buscarProducto(){
+            axios.get(config.server+"/loteP/id/"+this.id_producto)
+            .then((result)=>{
+                if(result.data.success && result.data.body.length>0){
+                    const pd = this.prods_venta.find(ele => ele.id_product == result.data.body[0].id_product);
+                    if(pd){
+                        if(pd.cant_venta<pd.cantidad_lote){
+                            pd.cant_venta++;
+                        }
+                    }else{
+                        let prod = Object.assign({cant_venta: 1}, result.data.body[0]);
+                        this.prods_venta.push(prod);
+                    }
+                    this.calcularTotal();
+                    this.id_producto = "";
+                }else{
+                    Swal.fire({
+                        icon: "info",
+                        title: "Producto no encontrado",
+                        showConfirmButton: true,
+                    });
+                }
+            }).catch((err)=>{
+                console.log(err);
+                Swal.fire({
+                    icon: "info",
+                    title: "Producto no encontrado",
+                    showConfirmButton: true,
+                });
+            })
+        },
+        agregarProd(i){
+            const pd = this.prods_venta.find(ele => ele.id_product == this.prods_filtro[i].id_product);
+            if(pd){
+                if(pd.cant_venta<pd.cantidad_lote){
+                    pd.cant_venta++;
+                }
+            }else{
+                let prod = Object.assign({cant_venta: 1}, this.prods_filtro[i]);
+                this.prods_venta.push(prod);
+            }
+            this.calcularTotal();
+            this.id_producto = "";
+            this.filtrado = false;
+        },
+        sumCantidad(id){
+            console.log(id);
+            console.log(this.prods_venta);
+            const pd = this.prods_venta.find(ele => ele.id_product == id);
+            if(pd.cant_venta<pd.cantidad_lote){
+                pd.cant_venta++;
+                this.calcularTotal()
+            }
+        },
+        resCantidad(id){
+            const pd = this.prods_venta.find(ele => ele.id_product == id);
+            if(pd.cant_venta>1){
+                pd.cant_venta--;
+                this.calcularTotal()
+            }
+        },
+        eliminarProd(id){
+            this.prods_venta.splice(id, 1);
+            this.calcularTotal()
+        },
+        calcularTotal(){
+            let total = 0;
+            for (let index = 0; index < this.prods_venta.length; index++) {
+                const element = this.prods_venta[index];
+                total+=element.precio_venta*element.cant_venta
+            }
+            this.total = total;
+        },
+        registrarVenta(saldo){
+            if(this.prods_venta.length>0 && this.cliente.cedula_cli != ''){
+                let productos = [];
+                for (let index = 0; index < this.prods_venta.length; index++) {
+                    const element = this.prods_venta[index];
+                    productos.push([element.id_lote, element.cant_venta, element.precio_venta]);
+                }
+                let ced = this.cliente.cedula_cli;
+                let form = {
+                    productos,
+                    cedula_cli: ced
+                }
+                axios.post(config.server+"/venta", form)
+                .then((result)=>{
+                    if (result.data.success) {
+                        if(saldo){
+                            this.$router.push({ path: '/RegistrarSaldo', query: {id_venta: result.data.body.id_venta, total_venta: result.data.body.total_venta}});
+                        }else{
+                            Swal.fire({
+                            icon: "success",
+                            title: "Venta realizada exitosamente",
+                            showConfirmButton: false,
+                            timer: 1000,
+                        });
+                        }
+                    } else {
+                        Swal.fire({
+                            icon: "error",
+                            title: "No se ha podido realizar la venta",
+                            showConfirmButton: false,
+                            timer: 1200,
+                        });
+                    }
+                }).catch((err) => {
+                    Swal.fire({
+                        icon: "error",
+                        title: "No se ha podido realizar la venta",
+                        showConfirmButton: false,
+                        timer: 1200,
+                    });
+                })
+                this.prods_venta = [];
+                this.filtrado = false;
+                this.id_producto = ""; 
+                this.calcularTotal();
+            }else{
+                if(this.cliente.cedula_cli==''){
+                    Swal.fire({
+                        icon: "info",
+                        title: "No se ha agregado un cliente",
+                        showConfirmButton: true,
+                    });
+                }
+                if(!(this.prods_venta.length>0)){
+                    Swal.fire({
+                        icon: "info",
+                        title: "No se han agregado productos",
+                        showConfirmButton: true,
+                    });
+                }
+            }
+        },
+        formatoMoneda(valor) {
+        const formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            minimumFractionDigits: 2,
+            currency: 'COP'
+        }) 
+        return formatter.format(valor)
+        },
+        buscarCliente(){
+            axios.get(config.server+"/cliente/cedula/"+this.cedula_cli)
+            .then((result)=>{
+                if(result.data.success && result.data.body.length > 0){
+                    this.cliente = result.data.body[0];
+                    this.clienteExiste = false;
+                }else{
+                    this.cliente = {
+                        cedula_cli: '',
+                        nombre_cli: ''
+                    }
+                    this.clienteExiste = true;
+                }
+            }).catch((err)=>{
+                console.log(err);
+                this.cliente = {
+                        cedula_cli: '',
+                        nombre_cli: ''
+                    }
+                this.clienteExiste = true;
+            })
+        },
+        goCliente(){
+            this.ClienteForm.cedula_cli = this.cedula_cli;
+            this.showModal = true;
+        },
+        
+        registrarCliente(){
+            axios.post(config.server+"/cliente", this.ClienteForm)
+            .then((result) => {
+                    if (result.data.success) {
+                        Swal.fire({
+                            icon: "success",
+                            title: "Cliente creado exitosamente",
+                            showConfirmButton: false,
+                            timer: 1000,
+                        });
+                        this.showModal = false;
+                    } else {
+                        Swal.fire({
+                            icon: "error",
+                            title: "No se ha podido crear el cliente",
+                            showConfirmButton: false,
+                            timer: 1200,
+                        });
+                    }
+                }).catch((err) => {
+                    Swal.fire({
+                        icon: "error",
+                        title: "No se ha podido crear el cliente",
+                        showConfirmButton: false,
+                        timer: 1200,
+                    });
+                })
+        }
+    },
 }
 </script>
 
 <style>
+.cursor{
+    cursor: pointer;
+}
     .factura{
         margin: auto;
         background-color: #ffffff;
